08f3feb26b5f0699eca0449a02eeec2a943433b3
apeedks <apeedks@gmail.com>
2026-02-20 03:48:44 -0500

    Add files via upload

---
+++ secure.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SecureurlPipe } from './secure.pipe';
+
+describe('SecurePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SecurePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
